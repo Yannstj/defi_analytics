8e7c212a94c69f78a40d0a4bd647b1fb12853d4c
Yannstj <yannick.saintjust@gmail.com>
2026-04-05 15:37:34 +0200

    go postgres connexion

--- defi_backend/main.go
+++ defi_backend/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/jackc/pgx/v5"
@@ -10,10 +11,14 @@ import (
 
 func main() {
 	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
 	}
 	defer conn.Close(context.Background())
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello defi")})
+	http.ListenAndServe(":8080", nil);
 
 }
